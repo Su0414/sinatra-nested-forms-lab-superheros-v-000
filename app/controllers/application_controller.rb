57a947bf2ae313b031ba9aef40389ce2a23211d1
Suchitra Naik <susanaik@gmail.com>
2018-06-19 15:37:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,15 @@ class App < Sinatra::Base
       raise params.inspect
 
       @team = Team.new(params[:team][:name],
-                      params[:team][:motto]
-                      )
+                      params[:team][:motto])
       @team.save
+
+      @hero1 = Members.new(params[:team][:members][0][:name],
+                       params[:team][:members][0][:power],
+                       params[:team][:members][0][:bio])
+      @hero1.save
+
+      
       erb :super_hero
     end
 
