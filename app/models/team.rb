b79f05fa1f4457c3688de7db6120cfd1104d0408
Suchitra Naik <susanaik@gmail.com>
2018-06-19 15:31:40 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,13 +1,20 @@
 class Team
   attr_accessor :name, :motto
-  @@all = []
+  @@teams = []
 
   def initialize(name, motto)
     @name = name
     @motto = motto
+    save
+  end
+
+  def save
+    @@teams << self
+    @@teams
   end
 
   def self.all
+    @@teams
   end
 
 end
